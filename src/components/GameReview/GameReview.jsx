b85bb72680f7abd8da5ce397344e60df3a9606b5
Daamin909 <ashai.daamin@gmail.com>
2025-03-14 15:25:11 +0530

    added: move eval card, report card

--- src/components/GameReview/GameReview.jsx
+++ src/components/GameReview/GameReview.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReportCard from "../ReportCard/ReportCard";
 import Accuracy from "../Accuracy/Accuracy";
 import "./GameReview.css";
+import Moves from "../Moves/Moves";
 const GameReview = ({ reviewedGame }) => {
   return (
     <div className="game_review">
-      <Accuracy PGN={reviewedGame} />
-      <ReportCard move_numbers={reviewedGame.number_of_move_types} />
+      <div className="acc_rep_box">
+        <Accuracy PGN={reviewedGame} />
+        <ReportCard move_numbers={reviewedGame.number_of_move_types} />
+      </div>
+      <Moves moves={reviewedGame.move_evaluations}/>
     </div>
   );
 };
